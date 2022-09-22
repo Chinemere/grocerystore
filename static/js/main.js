cb8e3cd8d4cfdfaa4d4cbd11b809f21b763cc66b
Chinemere Chukwu <chukwuchinemere@gmail.com>
2022-09-22 11:47:56 +0100

    The grocery store app

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+function check_me(input_id){
+    let checked_input= document.querySelector("input[id=" + input_id + "]");
+    let checked_label = document.querySelector("label[name=" + input_id + "]");
+    if(checked_input.checked){
+        checked_label.style.textDecoration="line-through";
+    }
+    else{
+        checked_label.style.textDecoration="" ;
+    }
+    let btn =document.getElementById("remove_btn")
+    btn.value = "REMOVE ITEM"
+    btn.style.color ="#ffffff"
+    btn.style.backgroundColor = "#FE7575"
+    btn.style.cursor = "pointer"
+}
